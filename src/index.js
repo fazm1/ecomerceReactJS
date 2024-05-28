a065a6dc0f17a1dc195eb3ca0a2cf08eefbeb76d
Farid Azzam <farid.azzam.2001@gmail.com>
2024-05-28 21:16:16 +0300

    index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { HashRouter } from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8080/graphql',
+    uri: 'https://ecommerce-fazm01-scandiweb.000webhostapp.com/src/Controller/graphql.php',
     cache: new InMemoryCache(),
 })
 
